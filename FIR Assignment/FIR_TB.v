2c3855cbc456efbf80dc298b57ece31691bd8a1f
Raja Aadhithan <raja.aadhithan.t@gmail.com>
2021-12-08 15:34:41 +0530

    fir

--- FIR Assignment/FIR_TB.v
+++ FIR Assignment/FIR_TB.v
@@ -19,5 +19,16 @@ module fir_tb;
 		
         x = 4; #50; // x[n-9] to x[n-5] will have the input 3, x[n-4] to x[n] will have input 4
 		$display("for x = %0d y is %0d",x,y);
+		
+		x = 124; #50; // for 1st 5 cycles 
+		x = 1116988; #50; // for 2nd 5 cycles
+		$display("for x = %0d y is %0d",x,y);
+		
+		x = 42; #20; // for 2 cycles
+		x = 124; #30; // for 3 cycles
+		x = 34; #20; // for 2 cycles
+		x = 67; #20; // for 2 cycles
+		x = 1116988; #10; // for 1 cycle
+		$display("for x = %0d y is %0d",x,y);
 	end
 endmodule
